7e60c4f3e9f43f498920b1f70951b692b61f1c00
Ian Zhang <ian.chen.zhang@gmail.com>
2020-04-05 12:36:39 -0700

    added google takeout data to map

--- js/travel-map.js
+++ js/travel-map.js
@@ -1,4 +1,5 @@
 var map;
+var markers = [];
 
 function initMap() 
 {
@@ -12,8 +13,9 @@ function initMap()
   var script = document.createElement('script');
   // This example uses a local copy of the GeoJSON stored at
   // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
-  script.src = "/js/2020_callback.js";
+  script.src = "/js/location_callback.js";
   document.getElementsByTagName('head')[0].appendChild(script);
+
 }
 
 // Loop through the results array and place a marker for each
@@ -22,10 +24,12 @@ window.location_array = function(results)
 {
   for (var i = 0; i < results.features.length; i++) {
     var coords = results.features[i].geometry.coordinates;
-    var latLng = new google.maps.LatLng(coords[1],coords[0]);
-    var marker = new google.maps.Marker({
+    var latLng = new google.maps.LatLng(coords[0],coords[1]);
+    markers.push(new google.maps.Marker({
       position: latLng,
-      map: map
-    });
+    }));
   }
+
+  var markerCluster = new MarkerClusterer(map, markers, {imagePath: '/img/map/m'});
+
 }
